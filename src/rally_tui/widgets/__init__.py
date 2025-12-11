c2e235dc6e6fec2d0e9b7dd5717b34260c3de9d0
Dan Elliott <dan-elliott-appneta@users.noreply.github.com>
2025-12-10 19:17:56 -0800

    Export CommandBar from widgets package

--- src/rally_tui/widgets/__init__.py
+++ src/rally_tui/widgets/__init__.py
@@ -1,6 +1,7 @@
 """TUI Widgets for Rally."""
 
+from .command_bar import CommandBar
 from .ticket_detail import TicketDetail
 from .ticket_list import TicketList, TicketListItem
 
-__all__ = ["TicketDetail", "TicketList", "TicketListItem"]
+__all__ = ["CommandBar", "TicketDetail", "TicketList", "TicketListItem"]
